d0d0f1de2d9fbc8490aecbd8fe4fdf0e1238eb13
Roberto Almanza <almanza.rbto@gmail.com>
2020-01-26 23:20:30 -0400

    Proyecto inicial

---
+++ src/main/java/org/solid/duck/Main.java
@@ -0,0 +1,7 @@
+package org.solid.duck;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SimUDuck 2");
+    }
+}
---
+++ src/main/java/org/solid/duck/simuduck/Main.java
@@ -0,0 +1,7 @@
+package org.solid.duck.simuduck;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SimUDuck");
+    }
+}
